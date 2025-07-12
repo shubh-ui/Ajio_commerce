a2698e190574313fcf77aefa3e55aa4df2cb9a4d
Shubham <shubhpatil8510@gmail.com>
2025-07-13 02:07:35 +0530

    added header for the search result.

--- ajio/src/app/products/page.tsx
+++ ajio/src/app/products/page.tsx
@@ -1,24 +1,57 @@
 'use client'
 
+import { useState } from "react"
 import Container from "../component/container/Container"
 import FilterBar from "../component/FilterBar/FilterBar"
-
+import { data } from "../utils/data"
 const page = () => {
+    console.log(data)
+    const [bigInfo, setBigInfo] = useState(false);
+
+    const hadleInfo = () => {
+        setBigInfo(prev => !prev);
+    }
+
     return (
         <Container>
-               <div className="h-[40px]">
+            <div className="h-[40px]">
 
-                </div>
+            </div>
             <div className="flex w-full">
-             
+
 
                 <div className="w-[20%]">
                     <FilterBar />
                 </div>
 
 
-                <div className="w-[80%] bg-gray-100 p-4">
-                    Main Content Area
+                <div className="w-[80%] p-4">
+                    <div className="info flex flex-col items-center">
+                        <h1>
+                            <div className="header1">
+                                {data.categoryforheader}
+                            </div>
+                            <div className="header2">
+                                {data.categoryName}
+                            </div>
+                        </h1>
+                        <div className="info-desc">
+                            <div
+                                className="prose text-sm prose-lg max-w-none"
+                                dangerouslySetInnerHTML={{ __html: data.categoryDescription }}
+                                style={{
+                                    // Override inline styles for better appearance
+                                    fontFamily: 'inherit',
+                                    lineHeight: '1.6',
+                                    height: bigInfo ? '400px' : '70px',
+                                    overflow: 'hidden'
+                                }}
+                            />
+                            <div>
+                                <a onClick={hadleInfo} style={{color: '#176d93;'}}>{bigInfo ? 'Show less' :'Show more'}</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Container>
