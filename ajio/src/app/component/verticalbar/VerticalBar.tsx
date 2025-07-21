53b03e934e8960b68a45143ff3eb21afbb95d390
Shubham <shubhpatil8510@gmail.com>
2025-07-21 23:37:02 +0530

    Css cahnges for vertical bars

--- ajio/src/app/component/verticalbar/VerticalBar.tsx
+++ ajio/src/app/component/verticalbar/VerticalBar.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const VerticalBarsUI = () => {
+const VerticalBarsUI = ({onChageGrid, grid} : {onChageGrid: any, grid : number}) => {
   return (
     <div className="flex items-center justify-center bg-gray-50">
       <div className="flex gap-1">
         {/* First container with 3 bars */}
         <div className="border border-gray-300 bg-white px-1 py-0.5">
-          <div className="flex gap-0.5">
+          <div className="flex gap-0.5 cursor-pointer" onClick={() => onChageGrid(3)}>
             {[1, 2, 3].map((bar) => (
               <div
                 key={bar}
-                className="w-1 h-3 bg-gray-400"
+                className={`w-1 h-3 ${grid == 3 ? 'bg-gray-400' : 'bg-gray-200'}`}
               ></div>
             ))}
           </div>
@@ -18,11 +18,11 @@ const VerticalBarsUI = () => {
         
         {/* Second container with 5 bars */}
         <div className="border border-gray-300 bg-white px-1 py-0.5">
-          <div className="flex gap-0.5">
+          <div className="flex gap-0.5 cursor-pointer" onClick={() => onChageGrid(5)}>
             {[1, 2, 3, 4, 5].map((bar) => (
               <div
                 key={bar}
-                className="w-1 h-3 bg-gray-400"
+                className={`w-1 h-3 ${grid == 5 ? 'bg-gray-400' : 'bg-gray-200'}`}
               ></div>
             ))}
           </div>
