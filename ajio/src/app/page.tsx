c1c97c742db39d75543dc3524aa5c2b8fda8c1d9
Shubham <shubhpatil8510@gmail.com>
2025-07-22 23:34:26 +0530

    Home page UI added

--- ajio/src/app/page.tsx
+++ ajio/src/app/page.tsx
@@ -1,9 +1,23 @@
 import Image from 'next/image'
 import Navbar from './component/Navbar/Navbar'
+import ImageSlider from './component/ImageSlider/ImageSlider'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <ImageSlider />
+      <div>
+        <div className='flex items-center justify-around py-24'> 
+          <div className='rounded-xl overflow-hidden'>
+              <Image className='scale-100 hover:scale-105 transition-transform' src="/images/banner1.png" width={600} height={200} alt='baner-image'/>
+          </div>
+
+          <div className='rounded-xl overflow-hidden'>
+              <Image className='scale-100 hover:scale-105 transition-transform' src="/images/banner2.png" width={600} height={200} alt='baner-image'/>
+
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
