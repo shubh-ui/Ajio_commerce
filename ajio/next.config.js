ba050a7a04395818441c85e2b5f7403f99caff0d
Shubham <shubhpatil8510@gmail.com>
2025-07-22 23:32:05 +0530

    Added img domain into next config

--- ajio/next.config.js
+++ ajio/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+      images: {
+    domains: ['assets.ajio.com'],
+    // Or use remotePatterns for more control (Next.js 12.3+)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.ajio.com',
+        port: '',
+        pathname: '/medias/**',
+      },
+    ],
+  },
+
+}
 
 module.exports = nextConfig
