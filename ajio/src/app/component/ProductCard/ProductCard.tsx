f3f2293b968c3d0c973f0c85db44dbc9d4de5750
Shubham <shubhpatil8510@gmail.com>
2025-07-22 23:35:09 +0530

    width and height chages for product card.

--- ajio/src/app/component/ProductCard/ProductCard.tsx
+++ ajio/src/app/component/ProductCard/ProductCard.tsx
@@ -5,7 +5,7 @@ import { BadgePercent, IndianRupeeIcon } from "lucide-react"
 const ProductCard = ({ product }: any) => {
     // console.log(truncateText(product.fnlColorVariantData.brandName))
     return (
-        <div className="w-[172px] flex flex-col gap-2.5 cursor-pointer">
+        <div className="w-full h-full flex flex-col gap-2.5 cursor-pointer">
             <div className="relative">
                 <img style={{ objectFit: 'scale-down' }} className="scale-100 hover:scale-105 transition-transform" src={product.fnlColorVariantData.outfitPictureURL} alt="" />
                 {(Number(product.averageRating) > 4 ) && <div className="absolute top-0 left-0 font-light text-[10px] m-2 p-1 bg-[#202020] text-[#fff] rounded-[6px]">BESTSELLER</div>}
